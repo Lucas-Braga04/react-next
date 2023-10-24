c1c82328a1d576bab1bb7c76e62a9e8bd066ed7f
Lucas-Braga04 <115594692+Lucas-Braga04@users.noreply.github.com>
2023-10-23 21:36:34 -0300

    att

--- exercicios/pages/render/componenteComFilho.jsx
+++ exercicios/pages/render/componenteComFilho.jsx
@@ -1,1 +1,18 @@
+import Item from "@/components/Item";
 import Lista from "@/components/Lista";
+
+export default function componenteComFilho(){
+    return(
+        <div>
+            <Lista>
+                <Item conteudo = "item #1"/>
+                <Item conteudo = "item #2"/>
+                <Item conteudo = "item #3"/>
+                <Item conteudo = "item #11"/>
+                <Item conteudo = "item #12"/>
+                <Item conteudo = "item #13"/>
+            </Lista>
+        </div>    
+    )
+
+}
